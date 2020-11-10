c3f3e7e4f361c686e480c8f7e609c60da745a6ff
Santi Paprika <santipaprika@users.noreply.github.com>
2020-11-10 22:38:42 +0100

    Pear interaction added.

--- Assets/Pear.cs
+++ Assets/Pear.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Pear : MonoBehaviour
 {
+    [SerializeField]
+    bool interactable = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +16,30 @@ public class Pear : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-   
+        if (!interactable) return;
+
+        bool buttonDownA, buttonDownB;
+        buttonDownA = buttonDownB = false;
+
+#if UNITY_EDITOR 
+        buttonDownA = Input.GetKeyDown("a");
+#else
+        buttonDownA = Input.GetButtonDown("A");
+#endif
+
+        if (buttonDownA) {
+            transform.Translate(new Vector3(-0.4f, 0, 0));
+        }
     }
 
-    public void DebugInfo() {
-        Debug.Log("This is a Pear :D");
+    public void AllowInteraction() {
+        interactable = true;
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    public void RemoveInteraction() {
+        interactable = false;
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
     }
 
 }
