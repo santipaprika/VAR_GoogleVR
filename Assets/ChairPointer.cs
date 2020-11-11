f82dc169e84e075e65bff7dd6a39595fead9e2ae
Rapese <raquelperezserra@gmail.com>
2020-11-11 13:16:25 +0100

    Floor and chair interaction added
    Co-Authored-By: Santi Paprika <44426596+santipaprika@users.noreply.github.com>

---
+++ Assets/ChairPointer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairPointer : MonoBehaviour
+{
+
+    //movement speed in units per second
+    private float movementSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PointerEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void PointerExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    public void PointerClick()
+    {
+        Destroy(gameObject);
+        //get the Input from Horizontal axis
+        ///float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        ///float verticalInput = Input.GetAxis("Vertical");
+        ///transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
+    }
+}
