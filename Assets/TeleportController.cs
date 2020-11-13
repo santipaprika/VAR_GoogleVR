fc5f8c5204ee3e312974ac866e7227824cf9790b
Rapese <raquelperezserra@gmail.com>
2020-11-13 14:03:59 +0100

    TeleportController added (with PointerClick)
    fer-ho per un boto

---
+++ Assets/TeleportController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TeleportController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    void Update()
+    {
+
+        bool buttonDownB;
+        buttonDownB = false;
+
+#if UNITY_EDITOR 
+        buttonDownB = Input.GetKeyDown("b");
+#else
+        buttonDownB = Input.GetButtonDown("B");
+#endif
+
+
+    }
+  
+    public void PointerClick()
+    {
+        player.transform.position = new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z);
+    }
+
+}
