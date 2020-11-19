5068c1436c4da88d8cd9ed9d23785b6fda4d2c33
Rapese <raquelperezserra@gmail.com>
2020-11-19 21:19:28 +0100

    more objects + David

---
+++ Assets/davidTalk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class davidTalk : InteractableItem
+
+{
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
